96f5c721be7321a1e456107cfa56d66319fa6bf8
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-09 20:31:51 +0200

    Create posts controller

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -1,19 +1,71 @@
-export const getPosts = (req, res) => {
-  res.json({});
+// Module dependencies
+import mongoose from 'mongoose';
+import Post from '../models/post';
+
+const ObjectId = mongoose.Types.ObjectId;
+
+export const getPosts = (req, res, next) => {
+  // Get filter from req
+  Post.find({})
+    .populate('author', 'username')
+    .then(posts => res.status(200).json(posts))
+    .catch(err => next(err));
 }
 
-export const getPost = (req, res) => {
-  res.json({});
+export const getPost = (req, res, next) => {
+  // Get post by id
+  const { id } = req.params;
+
+  if(!ObjectId.isValid(id)) {
+    res.status(404).json({ message: 'Not found' });
+    return next(new Error('Invalid post id'));
+  }
+
+  Post.findOne({ _id: id })
+    .populate('author', 'username')
+    .then(post => res.status(200).json(post))
+    .catch(err => next(err));
 }
 
-export const createPost = (req, res) => {
-  res.json({});
+export const createPost = (req, res, next) => {
+  console.log(req.body);
+  const { title, content, cover } = req.body;
+  const author = req.user._id;
+  // User must be authenticated. We can take user id
+  // from req.user
+  Post.create({ title, content, cover, author })
+    .then(post => res.status(201).json(post))
+    .catch(err => next(err));
 }
 
-export const updatePost = (req, res) => {
-  res.json({});
+export const updatePost = (req, res, next) => {
+  const { id } = req.params;
+  const { title, content, cover } = req.body;
+  const user = req.user._id;
+  // Check if authenticated user is the
+  // post author.
+  Post.findById(id)
+    .then(post => {
+      if(!post.author.equals(user)) {
+        res.status(401).json({ message: "You can't edit this post" });
+        throw 'Access Denied - Editing post';
+      }
+      return post;
+    })
+    .then(post => Post.updateOne({ _id: post._id }, { title, content, cover, updatedAt: Date.now() }, { new: true }))
+    .then(post => res.status(200).json(post))
+    .catch(err => next(err));
 }
 
-export const deletePost = (req, res) => {
-  res.json({});
+export const deletePost = (req, res, next) => {
+  const { id } = req.params;
+
+  if(!ObjectId.isValid(id)) {
+    res.status(404).json({ message: 'Not found' });
+    throw new Error('Invalid post id')
+  }
+
+  Post.deleteOne({ _id: id })
+    .then(result => res.status(200).json(result))
+    .catch(err => next(err));
 }
