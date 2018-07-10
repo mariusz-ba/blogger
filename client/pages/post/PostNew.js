7de3ed1a881302480a4fa8e9df14876edc89bfcd
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-10 13:32:23 +0200

    Create new post page

--- client/pages/post/PostNew.js
+++ client/pages/post/PostNew.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-export default class PostNew extends Component {
+class PostNew extends Component {
+  state = {
+    title: '',
+    cover: '',
+    content: '',
+    errors: null
+  }
+
+  changeTitle = e => {
+    this.setState({ title: e.target.value });
+  }
+
+  changeCover = e => {
+    this.setState({ cover: e.target.value });
+  }
+
+  changeContent = e => {
+    this.setState({ content: e.target.value });
+  }
+
+  submit = e => {
+    e.preventDefault();
+    // Publish post
+    const { title, cover, content } = this.state;
+    axios.post('/api/posts', { title, cover, content })
+      .then(res => this.props.history.push('/'))
+      .catch(err => this.setState({ errors: err.response.data.errors }));
+  }
+
   render() {
+    const { title, cover, content } = this.state;
+
     return (
-      <h1>PostNew</h1>
+      <div>
+        <form>
+          <label htmlFor="input-title">Title</label>
+          <input name="title" value={title} onChange={this.changeTitle} id="input-title"/>
+          <label htmlFor="input-cover">Cover</label>
+          <input name="cover" value={cover} onChange={this.changeCover} id="input-cover"/>
+          <label htmlFor="input-content">Content</label>
+          <input name="content" value={content} onChange={this.changeContent} id="input-content"/>
+          <button type="submit" onClick={this.submit}>Publish</button>
+        </form>
+      </div>
     )
   }
 }
+
+export default withRouter(PostNew);
