c32ee49cd5168db3d81bb0b5040c5c80ab572dab
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-21 18:52:37 +0200

    Add avatar styles

--- client/components/avatar/index.js
+++ client/components/avatar/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const style = {
+  width: 32,
+  height: 32,
+  borderRadius: '50%'
+}
+
 export default class Avatar extends Component {
   render() {
     const { img } = this.props;
 
     return (
-      <img className="avatar" src={img} alt="Avatar"/>
+      <img style={style} src={img} alt="Avatar"/>
     )
   }
 }
